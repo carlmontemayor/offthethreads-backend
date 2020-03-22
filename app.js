e8b0c678633d7bf08d526b8fd223c761f59060c0
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-03-21 18:38:40 -0700

    allowed all

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://api.offthethreads.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
