c87dd6caa307a0bdefa2e706837dca048dda2849
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-03-21 18:30:19 -0700

    added more cors

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 app.options('*', cors());
-app.use(cors());
+app.use(cors({origin: '*'}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // Only use logger when testing
 if (process.env.NODE_ENV !== 'test') {
