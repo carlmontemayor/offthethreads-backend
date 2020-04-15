eb24abfeda63f9ea3f431dff07a061921f27efb5
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-04-15 01:10:50 -0700

    added server side rendering

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var clothingRouter = require('./routes/clothing');
 var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
 
 // Load env variables
 require('dotenv').config();
@@ -14,10 +15,10 @@ require('dotenv').config();
 var app = express();
 app.options('*', cors());
 app.use(cors({origin: '*'}));
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
 // Only use logger when testing
 if (process.env.NODE_ENV !== 'test') {
@@ -48,9 +49,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'))
 
 app.use('/v1/clothing', clothingRouter);
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
