2837b1c8eb7e96ab343ff8f1ef14774285131e91
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-04-19 16:09:44 -0700

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,6 +15,7 @@ require('dotenv').config();
 
 var app = express();
 
+app.use(cors());
 app.use(helmet());
 
 // view engine setup
