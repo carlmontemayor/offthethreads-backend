84f99f4eb9cf1b6e52651150217980acf3795eaf
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-04-19 15:32:13 -0700

    changed cors

--- app.js
+++ app.js
@@ -16,28 +16,10 @@ require('dotenv').config();
 var app = express();
 
 app.use(helmet());
-app.use(cors());
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors());
+}
 
 // view engine setup
 app.set('views', __dirname + '/views');
