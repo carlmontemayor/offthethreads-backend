7586f2b556add0a6df4b3a1f21611a43eae206da
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-04-21 13:44:12 -0700

    reordered routes to fix render issue

--- routes/inventory.js
+++ routes/inventory.js
@@ -6,7 +6,7 @@ var queries = require('../db/queries');
 router.get('/', function(req, res) {
     queries.getAll()
         .then((clothing) => {
-            res.send(clothing);
+            res.status(200).render('inventory', {clothing: clothing})
         })
         .catch((error) => {
             next(error);
